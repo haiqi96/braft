c7e45d27d771088460caeb9dd9357d8bcffe44c9
Jack <14502009+haiqi96@users.noreply.github.com>
2022-04-12 22:23:09 +0000

    add dummy sender for experiment

--- example/experiment/client.cpp
+++ example/experiment/client.cpp
@@ -49,6 +49,51 @@ struct client_args{
 //     return hash;
 // }
 
+
+static void *dummy_sender(void *arg)
+{
+
+    struct client_args * cl_args = (struct client_args *)arg;
+    std::cout << cl_args->op <<" : "<<cl_args->key<<" : "<<cl_args->value<<std::endl; 
+    
+    // Send to the leader group
+    boost::hash<std::string> stringHash;
+    int hash_code  = stringHash(cl_args->key);
+    std::string replica_group = "replica_" + std::to_string(hash_code % FLAGS_num_groups);
+    while (!brpc::IsAskedToQuit())
+    {
+        // For any non-read operation call the stub.insert rpc call
+        if (OP_WRITE == cl_args->op || OP_DELETE == cl_args->op || OP_MODIFY == cl_args->op)
+        {
+            keyvalue::InsertRequest request;
+            keyvalue::InsertResponse response;
+
+            std::string test_key = cl_args->key;
+            std::string test_value = cl_args->value;
+            request.set_key(test_key);
+            request.set_op(cl_args->op);
+            request.set_value(test_value);
+            if (OP_DELETE == cl_args->op){
+                std::cout << "delete " << test_key << std::endl;
+            }
+            else {
+                std::cout << "write " << test_key << " : " <<  test_value << std::endl;
+            }
+        } 
+        else if(OP_READ == cl_args->op)
+        {
+            keyvalue::GetRequest request;
+            keyvalue::GetResponse response;
+
+            std::string read_test_key = cl_args->key;
+            printf("read key %s\n", read_test_key.c_str());
+        }
+        // If either operation is successful, break out of the loop and accept another op
+        break;
+    }
+    return NULL;
+}
+
 static void *sender(void *arg)
 {
 
@@ -195,6 +240,8 @@ static void *sender(void *arg)
     return NULL;
 }
 
+
+// Back up this main function in case we decide we need it
 int main(int argc, char *argv[])
 {
     GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
@@ -243,7 +290,7 @@ int main(int argc, char *argv[])
         {
             for (int i = 0; i < FLAGS_thread_num; ++i)
             {
-                if (pthread_create(&tids[i], NULL, sender, cl_args) != 0)
+                if (pthread_create(&tids[i], NULL, dummy_sender, cl_args) != 0)
                 {
                     LOG(ERROR) << "Fail to create pthread";
                     return -1;
@@ -254,7 +301,7 @@ int main(int argc, char *argv[])
         {
             for (int i = 0; i < FLAGS_thread_num; ++i)
             {
-                if (bthread_start_background(&tids[i], NULL, sender, cl_args) != 0)
+                if (bthread_start_background(&tids[i], NULL, dummy_sender, cl_args) != 0)
                 {
                     LOG(ERROR) << "Fail to create bthread";
                     return -1;
@@ -275,20 +322,110 @@ int main(int argc, char *argv[])
                 }
             }
     }
-
-    //while (!brpc::IsAskedToQuit())
-    //{
-    //    sleep(1);
-    //    LOG_IF(INFO, !FLAGS_log_each_request)
-    //        << "Sending Request to " << FLAGS_group
-    //        << " (" << FLAGS_conf << ')'
-    //        << " at qps=" << g_latency_recorder.qps(1)
-    //        << " latency=" << g_latency_recorder.latency(1);
-    //}
-
     // User pressed Ctrl + C
     LOG(INFO) << "Counter client is going to quit";
 
     free(cl_args);
     return 0;
 }
+
+
+
+// // Back up this main function in case we decide we need it
+// int main_backup(int argc, char *argv[])
+// {
+//     GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
+//     butil::AtExitManager exit_manager;
+
+//     // cl_args stored in heap because this pointer is passed to the sender thread
+//     struct client_args * cl_args = (struct client_args *) malloc(sizeof(struct client_args));
+
+//     // Register configuration of target group to RouteTable
+//     if (braft::rtb::update_configuration(FLAGS_group, FLAGS_conf) != 0)
+//     {
+//         LOG(ERROR) << "Fail to register configuration " << FLAGS_conf
+//                    << " of group " << FLAGS_group;
+//         return -1;
+//     }
+//     std::vector<bthread_t> tids;
+
+//     // Keep accepting operations until exit
+//     while (!brpc::IsAskedToQuit()){
+//         // Command line interface to accept arguments: op, key-value pair
+//         int cin_op;
+//         std::string cin_key;
+//         std::string cin_value;
+//         std::cout << "Enter op code (0, 1, 2 or 3) press -1 to exit: ";
+//         std::cin >> cin_op;
+
+//         // If input op is -1, exit the program
+//         if(cin_op == -1){
+//             free(cl_args);  
+//             return 0;
+//         }
+//         cl_args->op = cin_op;
+//         std::cout << "Enter the key :";
+//         std::cin >> cin_key;
+//         cl_args->key = cin_key;
+//         // Read and Delete operations don't require a value
+//         if((OP_READ != cin_op) &&  (OP_DELETE != cin_op)){
+//             std::cout << "Enter the value :";
+//             std::cin >> cin_value;
+//             cl_args->value = cin_value;
+//         }
+
+
+//         tids.resize(FLAGS_thread_num);
+//         if (!FLAGS_use_bthread)
+//         {
+//             for (int i = 0; i < FLAGS_thread_num; ++i)
+//             {
+//                 if (pthread_create(&tids[i], NULL, sender, cl_args) != 0)
+//                 {
+//                     LOG(ERROR) << "Fail to create pthread";
+//                     return -1;
+//                 }
+//             }
+//         }
+//         else
+//         {
+//             for (int i = 0; i < FLAGS_thread_num; ++i)
+//             {
+//                 if (bthread_start_background(&tids[i], NULL, sender, cl_args) != 0)
+//                 {
+//                     LOG(ERROR) << "Fail to create bthread";
+//                     return -1;
+//                 }
+//             }
+//         }
+
+//         // Wait until the threads end
+//         for (int i = 0; i < FLAGS_thread_num; ++i)
+//             {
+//                 if (!FLAGS_use_bthread)
+//                 {
+//                     pthread_join(tids[i], NULL);
+//                 }
+//                 else
+//                 {
+//                     bthread_join(tids[i], NULL);
+//                 }
+//             }
+//     }
+
+//     //while (!brpc::IsAskedToQuit())
+//     //{
+//     //    sleep(1);
+//     //    LOG_IF(INFO, !FLAGS_log_each_request)
+//     //        << "Sending Request to " << FLAGS_group
+//     //        << " (" << FLAGS_conf << ')'
+//     //        << " at qps=" << g_latency_recorder.qps(1)
+//     //        << " latency=" << g_latency_recorder.latency(1);
+//     //}
+
+//     // User pressed Ctrl + C
+//     LOG(INFO) << "Counter client is going to quit";
+
+//     free(cl_args);
+//     return 0;
+// }
