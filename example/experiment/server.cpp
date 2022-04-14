ad792ed20f5e827e3770921eba237f2f0a337241
Jack <14502009+haiqi96@users.noreply.github.com>
2022-04-14 21:47:42 +0000

    accept any request from any ip

--- example/experiment/server.cpp
+++ example/experiment/server.cpp
@@ -502,7 +502,8 @@ int main(int argc, char *argv[])
     // Notice the default options of server is used here. Check out details from
     // the doc of brpc if you would like change some options;
     std::string ip_and_port = FLAGS_ip + ":" + std::to_string(FLAGS_port);
-    if (server.Start(ip_and_port.data(), NULL) != 0)
+    butil::EndPoint point = butil::EndPoint(butil::IP_ANY, FLAGS_port)
+    if (server.Start(point, NULL) != 0)
     {
         LOG(ERROR) << "Fail to start Server";
         return -1;
